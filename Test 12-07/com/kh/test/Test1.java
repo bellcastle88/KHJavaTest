38bba426c3ca34113085a856e29237f6c71f42c9
Your Name <you@example.com>
2020-12-07 22:09:02 +0900

    Commit 12-07

---
+++ Test 12-07/com/kh/test/Test1.java
@@ -0,0 +1,24 @@
+package com.kh.test;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		
+		byte a = 100;
+		long b = 999900000000L;
+		float c = 486.520F;
+		double d = 567.890123;
+		char e ='A';
+		String f ="Hello world";
+		boolean g = true;
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+	}
+
+}
---
+++ Test 12-07/com/kh/test/Test2.java
@@ -0,0 +1,35 @@
+package com.kh.test;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("A = "+ ('A'+0));
+        
+        System.out.println("0 =" + ('0' + 0) );
+        
+        System.out.println("(char)65 = " + (char)65);
+        
+        byte bt1 = 125;
+        char ch1 = 'C';
+        int it1 = bt1 - ch1;
+        System.out.println("it1 = "+it1);
+        
+        
+        boolean bool = false;
+        
+        bool = (2.0 == 5/2);
+        System.out.println("bool = " + bool);
+        
+        bool = ('C' == 67);
+        System.out.println("bool = " + bool);
+        
+        bool = ('A' == 'B'-1);
+        System.out.println("bool = " + bool);
+        
+        bool = ('a' != 97 );//부정연산자
+        System.out.println("bool = " + bool);
+
+	}
+
+}
