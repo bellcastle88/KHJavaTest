690a50cc5f0c329ecfdcdaede5c382d275f1841d
Your Name <you@example.com>
2020-12-10 13:09:58 +0900

    complete All

--- Test 12/09/com/kh/test/Condition/Test5/java/test.java
+++ Test 12/09/com/kh/test/Condition/Test5/java/test.java
@@ -1,10 +1,46 @@
 package com.kh.test.Condition.Test5.java;
 
+import java.util.Scanner;
+
 public class test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		test t5 = new test();
+		t5.test();
 
+	}
+
+	public void test() {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("키(cm)를 입력하세요. (ex. 175) : ");
+		int height = sc.nextInt();
+		System.out.println("몸무게(kg)를 입력하세요. (ex. 65) : ");
+		double weight = sc.nextInt();
+
+		double bmi = weight / (((double) height / 100.0) * ((double) height / 100.0));
+		System.out.println("BMI = " + bmi);
+
+		if (bmi < 18.5) {
+			System.out.println("저체중");
+		} else if (bmi >= 18.5 && bmi < 23.0) {
+			System.out.println("정상체중");
+
+		} else if (bmi >= 23.0 && bmi < 25.0) {
+			System.out.println("과체중");
+
+		} else if (bmi >= 25 && bmi < 30) {
+			System.out.println("비만");
+
+		}
+
+		else if (bmi >= 30.0) {
+			System.out.println("고도비만");
+
+		}
+
+		sc.close();
 	}
 
 }
